d98c41f1b502aac61d41c0d1a8114de8405d7a5c
MMakinson <mmakinson80@gmail.com>
2021-11-22 10:45:39 -0800

    fixed the random fact generator :)

--- scripts/facts.js
+++ scripts/facts.js
@@ -1,11 +1,11 @@
 let sailingFacts = ['Titanic was the first ocean liner to have a swimming pool and a gym', 'The Americas Cup, a race for sailboats, was originally awarded in 1851, making it the oldest sporting trophy in the world', 'The youngest person to circumnavigate the world was 16-year old Laura Dekker', 'the ideal wind speed for sailing is between eight and twelve knots', 'The average salinity of oceans is 3.5%'];
 
 
-let randomFact = sailingFacts[Math.floor(Math.random()*sailingFacts.length)];
 
 module.exports = function (bot) {
     bot.hear(/(.*) sailing fact/i, function(res){
         let factType
+        let randomFact = sailingFacts[Math.floor(Math.random()*sailingFacts.length)];
         factType = res.match[1].toLowerCase();
         switch (factType) {
             case 'interesting':
@@ -18,7 +18,7 @@ module.exports = function (bot) {
                 return res.reply(`${sailingFacts[1]}`)
                 break
             case 'random':
-                return res.reply(`what is ${randomFact}`)
+                return res.reply(`${randomFact}`)
                 break
             default:
                 return res.reply(`You didn't choose one of the options! Have a random fact: ${randomFact}`)
